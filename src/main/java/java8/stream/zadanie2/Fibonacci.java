aa11b88ebd94a21c9423d09c0af524ba669ca8a3
Szymon <Szymon_Sadkowski@epam.com>
2018-05-16 17:01:23 +0200

    rozwiazane drugie

--- src/main/java/java8/stream/zadanie2/Fibonacci.java
+++ src/main/java/java8/stream/zadanie2/Fibonacci.java
@@ -12,9 +12,19 @@ public class Fibonacci {
                 .forEach(System.out::println);
     }
 
-    public static void main(String[] args) {
-        printFibonacci(10);
+    private static void modifiedPrintFibonacci(int howMany)
+    {
+        Stream
+                .iterate(new int[] {0, 1, 1}, f -> new int[] { f[1], f[2], f[0] + f[1] + f[2]})
+                .limit(howMany)
+                .map(f -> f[0])
+                .forEach(System.out::println);
+    }
 
+    public static void main(String[] args) {
         //TODO: Generate modified fibonacci sequence: each element is a sum of THREE (not two) preceding elements
+        printFibonacci(10);
+        System.out.println("-----------------------------");
+        modifiedPrintFibonacci(10);
     }
 }
