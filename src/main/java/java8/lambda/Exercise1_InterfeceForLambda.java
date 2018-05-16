7842498c69a8977a1a02a24a20297be176173b5b
Przemyslaw Jakubowski <przemyslaw_jakubowski@epam.com>
2018-05-16 17:17:55 +0200

    Exercise 1: change in description

--- src/main/java/java8/lambda/Exercise1_InterfeceForLambda.java
+++ src/main/java/java8/lambda/Exercise1_InterfeceForLambda.java
@@ -4,6 +4,6 @@ public class Exercise1_InterfeceForLambda {
 
     // Implement interface which is accepting description and list of integer values
     // Implement lambda based on this interface in a way that it is showing each value
-    //      from list followed by description.
-    //      It is also returning sum of elements. Print it out later
+    //      from list increased by 10, followed by description.
+    //      It is also returning sum of elements increased by 10. Print it out later
 }
