7bf1f71c2a497d10906d024de7e63300d7d92859
Przemyslaw Jakubowski <przemyslaw_jakubowski@epam.com>
2018-05-16 17:16:28 +0200

    Exercise 1: solution for exercise 1

--- src/main/java/java8/lambda/Exercise1_InterfeceForLambda.java
+++ src/main/java/java8/lambda/Exercise1_InterfeceForLambda.java
@@ -1,9 +1,29 @@
 package java8.lambda;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Exercise1_InterfeceForLambda {
 
     // Implement interface which is accepting description and list of integer values
     // Implement lambda based on this interface in a way that it is showing each value
-    //      from list followed by description.
-    //      It is also returning sum of elements. Print it out later
+    //      from list increased by 10, followed by description.
+    //      It is also returning sum of elements increased by 10. Print it out later
+
+    public static void main(String[] args) {
+        InterfaceForLambda interfaceForLambda = (description, integerList) -> {
+            int sum = 0;
+            for (Integer number : integerList){
+                System.out.println(description + (number + 10));
+                sum += number + 10;
+            }
+
+            return sum;
+        };
+
+        List<Integer> integerList = new ArrayList<>(Arrays.asList(1, 2, 3, 4, 5));
+
+        System.out.println("Sum of elements " + interfaceForLambda.doStuff("Element increased by 10: ", integerList));
+    }
 }
---
+++ src/main/java/java8/lambda/InterfaceForLambda.java
@@ -0,0 +1,8 @@
+package java8.lambda;
+
+import java.util.List;
+
+@FunctionalInterface
+public interface InterfaceForLambda {
+    int doStuff(String description, List<Integer> integerList);
+}
