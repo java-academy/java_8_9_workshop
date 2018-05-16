c09188e8c679fa6bb2a8075da66444051f33ee19
Przemyslaw Jakubowski <przemyslaw_jakubowski@epam.com>
2018-05-16 16:38:48 +0200

    Example1: Small difference between lambda and anonymous class

--- src/main/java/java8/lambda/Example1_SimpleInterface.java
+++ src/main/java/java8/lambda/Example1_SimpleInterface.java
@@ -13,6 +13,60 @@ public class Example1_SimpleInterface {
 
         simpleInterfaceAnonymous.doStuff("is doing stuff.");
         simpleInterfaceLambda.doStuff("is doing stuff.");
-        
+
+        SimpleInterface simpleInterfaceAnonymous1 = new SimpleInterface() {
+            @Override
+            public void doStuff(String string) {
+                System.out.println("Anonymous " + string);
+            }
+        };
+
+        SimpleInterface simpleInterfaceLambda1 = (s) -> System.out.println("Mighty lambda " + s);
+
+        SimpleInterface simpleInterfaceAnonymous2 = new SimpleInterface() {
+            @Override
+            public void doStuff(String string) {
+                System.out.println("Anonymous " + string);
+            }
+        };
+
+        SimpleInterface simpleInterfaceLambda2 = (s) -> System.out.println("Mighty lambda " + s);
+
+        SimpleInterface simpleInterfaceAnonymous3 = new SimpleInterface() {
+            @Override
+            public void doStuff(String string) {
+                System.out.println("Anonymous " + string);
+            }
+        };
+
+        SimpleInterface simpleInterfaceLambda3 = (s) -> System.out.println("Mighty lambda " + s);
+
+        SimpleInterface simpleInterfaceAnonymous4 = new SimpleInterface() {
+            @Override
+            public void doStuff(String string) {
+                System.out.println("Anonymous " + string);
+            }
+        };
+
+        SimpleInterface simpleInterfaceLambda4 = (s) -> System.out.println("Mighty lambda " + s);
+
+        SimpleInterface simpleInterfaceAnonymous5 = new SimpleInterface() {
+            @Override
+            public void doStuff(String string) {
+                System.out.println("Anonymous " + string);
+            }
+        };
+
+        SimpleInterface simpleInterfaceLambda5 = (s) -> System.out.println("Mighty lambda " + s);
+
+        SimpleInterface simpleInterfaceAnonymous6 = new SimpleInterface() {
+            @Override
+            public void doStuff(String string) {
+                System.out.println("Anonymous " + string);
+            }
+        };
+
+        SimpleInterface simpleInterfaceLambda6 = (s) -> System.out.println("Mighty lambda " + s);
+
     }
 }
