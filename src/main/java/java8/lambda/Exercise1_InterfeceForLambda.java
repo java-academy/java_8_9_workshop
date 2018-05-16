c9ce5b0ab59c5b480e7ab10569c06649f48cf94a
Przemyslaw Jakubowski <przemyslaw_jakubowski@epam.com>
2018-05-16 17:01:25 +0200

    Exercise 1: description for exercise 1 added

---
+++ src/main/java/java8/lambda/Exercise1_InterfeceForLambda.java
@@ -0,0 +1,9 @@
+package java8.lambda;
+
+public class Exercise1_InterfeceForLambda {
+
+    // Implement interface which is accepting description and list of integer values
+    // Implement lambda based on this interface in a way that it is showing each value
+    //      from list followed by description.
+    //      It is also returning sum of elements. Print it out later
+}
