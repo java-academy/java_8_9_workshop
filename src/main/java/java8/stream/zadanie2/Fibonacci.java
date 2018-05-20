341efe0c4d56504cd34a6f52948f1020d3e50592
jeger <szymon.sadkowski96@gmail.com>
2018-05-20 11:57:30 +0200

    rozwiazane 2, 3 do zrobienia

--- src/main/java/java8/stream/zadanie2/Fibonacci.java
+++ src/main/java/java8/stream/zadanie2/Fibonacci.java
@@ -5,7 +5,11 @@ import java.util.stream.Stream;
 public class Fibonacci {
     private static void printFibonacci(int howMany)
     {
-        Stream.iterate(new int[] {0, 1}, n -> new int[] {n[1], n[0] + n[1]}).map(n -> n[0]).limit(howMany).forEach(System.out::println);
+        Stream
+                .iterate(new int[] {0, 1}, n -> new int[] {n[1], n[0] + n[1]})
+                .map(n -> n[0])
+                .limit(howMany)
+                .forEach(System.out::println);
     }
 
     public static void main(String[] args) {
