7db8d9cecc84dba7db45fb27567a9531e1e78204
Przemyslaw Jakubowski <przemyslaw_jakubowski@epam.com>
2018-05-21 12:39:20 +0200

    Exericise 3 description

---
+++ src/main/java/java8/interfaces/Exercise3_Traveler.java
@@ -0,0 +1,9 @@
+package java8.interfaces;
+
+public class Exercise3_Traveler {
+    // 1. Create collection
+    // 2. Traverse collection using iterator
+    // 3. Traverse collection using collectionName.forEach() and anonymous class
+    // 4. Traverse collection using Consumer<Integer>
+    // 5. Traverse collection using Consumer<Integer> and show values increased by 10 in it
+}
