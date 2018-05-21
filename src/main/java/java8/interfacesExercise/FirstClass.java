92a6d08b30e9f81a11a816da12a2ce4e93ab4371
Przemyslaw Jakubowski <przemyslaw_jakubowski@epam.com>
2018-05-21 12:04:30 +0200

    Exericise 2 part 5

--- src/main/java/java8/interfacesExercise/FirstClass.java
+++ src/main/java/java8/interfacesExercise/FirstClass.java
@@ -2,8 +2,4 @@ package java8.interfacesExercise;
 
 public class FirstClass implements FirstInterface {
 
-    @Override
-    public void log(String toBePrinted) {
-        System.out.println("First class log print: " + toBePrinted);
-    }
 }
--- src/main/java/java8/interfacesExercise/FirstInterface.java
+++ src/main/java/java8/interfacesExercise/FirstInterface.java
@@ -1,7 +1,9 @@
 package java8.interfacesExercise;
 
 public interface FirstInterface {
-    default void log(String toBePrinted) {
+
+    // it is used to prevent from poor implementation
+    static void log(String toBePrinted) {
         System.out.println("First interface log print: " + toBePrinted);
     }
 }
