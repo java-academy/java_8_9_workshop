5df5872b228deb5a3cd77e56a682536179a8a850
Przemyslaw Jakubowski <przemyslaw_jakubowski@epam.com>
2018-05-21 12:43:43 +0200

    Solution exercise 3_1

--- src/main/java/java8/interfaces/Exercise3_Traveler.java
+++ src/main/java/java8/interfaces/Exercise3_Traveler.java
@@ -1,9 +1,28 @@
 package java8.interfaces;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class Exercise3_Traveler {
     // 1. Create collection
     // 2. Traverse collection using iterator
     // 3. Traverse collection using collectionName.forEach() and anonymous class
     // 4. Traverse collection using Consumer<Integer>
     // 5. Traverse collection using Consumer<Integer> and show values increased by 10 in it
+
+    // 1.
+    public static void main(String[] args) {
+
+        List<Integer> myList = new ArrayList<>();
+        for(int i=0; i<10; i++) myList.add(i);
+
+        myList = IntStream.range(0, 10)
+                .boxed()
+                .collect(Collectors.toList());
+
+    // 2.
+        
+    }
 }
