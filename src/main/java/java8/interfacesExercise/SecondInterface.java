bcf2b0c40e1f1c07cd49ceafa661c6a2b14e2ae9
Przemyslaw Jakubowski <przemyslaw_jakubowski@epam.com>
2018-05-21 12:01:09 +0200

    Exericise 2 part 4

--- src/main/java/java8/interfacesExercise/SecondInterface.java
+++ src/main/java/java8/interfacesExercise/SecondInterface.java
@@ -4,4 +4,10 @@ public interface SecondInterface {
     default void log(String toBePrinted) {
         System.out.println("Second interface log print: " + toBePrinted);
     }
+
+    // Method toString will be override by method from Object, so it is pointless to create default implementation of it
+
+//    default String toString(){
+//
+//    }
 }
